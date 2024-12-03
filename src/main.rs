af10d61d654d0061872fa368ec8979e3577c37c2
Sönke Hahn <soenkehahn@gmail.com>
2024-12-02 20:49:03 -0500

    Add command line flag for source code file

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,18 @@ mod fuse;
 mod tree;
 mod treesitter;
 
+use clap::Parser;
+use std::path::PathBuf;
+
+#[derive(Parser)]
+struct Args {
+    #[arg(long)]
+    path: PathBuf,
+}
+
 fn main() {
-    let tree = crate::treesitter::get_tree();
+    let args = Args::parse();
+    let tree = crate::treesitter::get_tree(&args.path);
     dbg!(&tree);
     crate::fuse::run(tree);
 }
